dadd9d56e3bd59c530a91833bd38b470e5aa731f
Vitaliy Prachov <vitaliy.prachov@gmail.com>
2023-04-12 00:30:18 +0300

    prepare for deployment

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import glsl from 'vite-plugin-glsl';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/visual-graphics-1/',
+  base: '/virtual-reality/',
   plugins: [glsl()]
 });
 
